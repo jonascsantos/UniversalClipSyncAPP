83b0c0d9d0528d8524c7dde4e85dd39ecaf51ed4
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-02-28 23:47:18 +0100

    button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Change Text"/>
     </View>
   );
 }
