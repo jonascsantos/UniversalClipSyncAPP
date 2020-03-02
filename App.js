7540c2bc475249adf8abb321f9a7dbe56749498a
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-03 00:22:30 +0100

    redo

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { StatusBar } from 'react-native';
-import { LinearGradient } from 'expo';
 
 export default function App() {
   return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <Text>AAA</Text>
+      </View>
+      <View>
+
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
