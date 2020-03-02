df546996e9408a494e55b4586c3dd4ef4fce7bcb
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-02 23:59:45 +0100

    add

--- App.js
+++ App.js
@@ -4,21 +4,12 @@ import { StatusBar } from 'react-native';
 import { LinearGradient } from 'expo';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to startaa working on your app!')
   return (
-    <View style={styles.container}>
-    <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
-      <Button title="Change Text Red" onPress={() => setOutputText('The text changed! RED')}/>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fefefe',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
