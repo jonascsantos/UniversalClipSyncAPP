d78207c7c8ae41633400a641bacdebd6bd87806b
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-03 18:07:42 +0100

    Flatlist goals

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   }
 
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal }
+    ]);
   }
 
   return (
@@ -18,13 +22,20 @@ export default function App() {
         <TextInput
           placeholder="Course Goal"
           style={styles.textInputADD}
-          onChangeText= {goalInputHandler}
+          onChangeText={goalInputHandler}
           value={enteredGoal}
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
-      </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItems}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -45,6 +56,14 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+
+  listItems: {
+    padding: 10,
+    backgroundColor: '#fefefe',
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'black'
   }
 
 });
