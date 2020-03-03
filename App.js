1180fc1d8d143127f904f8b5e69454d137425b63
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-03 15:47:42 +0100

    before stylesheet file

--- App.js
+++ App.js
@@ -4,14 +4,26 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 30 }}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
+      <View 
+        style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignContent: 'center'
+        }}
+      >
         <TextInput 
           placeholder="Course Goal" 
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
-        <Button title="ADD"></Button>
+          style={{ 
+              width: '80%', 
+              borderColor: 'black', 
+              borderWidth: 1, 
+              padding: 10
+          }}
+        />
+        <Button title="ADD">
+        </Button>
       </View>
       <View>
-
       </View>
     </View>
   );
