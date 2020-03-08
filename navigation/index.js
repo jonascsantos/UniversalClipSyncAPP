9db360147497e46545790065fa38e38a35f0b06b
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-09 00:49:52 +0100

    more files

---
+++ navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react'   
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+
+import Welcome from '../screens/Welcome';
+import Login from '../screens/Login';
+import Forgot from '../screens/Forgot';
+import Clipboard from '../screens/Clipboard';
+import Recents from '../screens/Recents';
+import Favourites from '../screens/Favourites';
+import Devices from '../screens/Devices';
+import Settings from '../screens/Settings';
