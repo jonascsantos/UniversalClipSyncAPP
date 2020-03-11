0fc8b399c5a4b993b4c6881db9e56a6f3e545fa5
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-11 23:54:23 +0100

    loading welcome page APP.js

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+import { AppLoading, Asset } from 'expo';
+
+import Navigation from './navigation'
+import { Block } from './components'
+
+//import used images
+const images = [
+  require('../assets/images/logo.png'),
+  require('../assets/images/onboarding1.png'),
+  require('../assets/images/onboarding2.png'),
+  require('../assets/images/onboarding3.png'),
+  require('../assets/images/phone.png'),
+  require('../assets/images/desktop.png'),
+  require('../assets/images/avatar.png'),
+];
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  }
+
+  handleResourcesAsync = async () => {
+
+    //caching images for better performance
+    const cacheImages = images.map(img => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  }
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen){
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
+
     <View style={styles.container}>
-      <Text>Universal Clip Sync APP</Text>
+      <Navigation />
     </View>
-  );
+  };
 }
 
 const styles = StyleSheet.create({
--- navigation/index.js
+++ navigation/index.js
@@ -4,25 +4,25 @@ import {Image} from 'react-native'
 import { createAppContainer, createStackNavigator } from 'react-navigation'
 
 import Welcome from '../screens/Welcome';
-import Login from '../screens/Login';
-import Forgot from '../screens/Forgot';
-import Clipboard from '../screens/Clipboard';
-import Recents from '../screens/Recents';
-import Favourites from '../screens/Favourites';
-import Devices from '../screens/Devices';
-import Settings from '../screens/Settings';
+// import Login from '../screens/Login';
+// import Forgot from '../screens/Forgot';
+// import Clipboard from '../screens/Clipboard';
+// import Recents from '../screens/Recents';
+// import Favourites from '../screens/Favourites';
+// import Devices from '../screens/Devices';
+// import Settings from '../screens/Settings';
 
 import { theme } from '../constants'
 
 const screens = createStackNavigator({
     Welcome,
-    Login,
-    Forgot,
-    Clipboard,
-    Recents,
-    Favourites,
-    Devices,
-    Settings
+    // Login,
+    // Forgot,
+    // Clipboard,
+    // Recents,
+    // Favourites,
+    // Devices,
+    // Settings
 },{
     defaultNavigationOptions: {
         headerStyle: {},
