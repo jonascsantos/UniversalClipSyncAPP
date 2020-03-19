041172c9df3944493010a958481e6fc9d17020b1
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-19 22:31:37 +0100

    clipboard

--- screens/Clipboard.js
+++ screens/Clipboard.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import { Keyboard, TextInput, KeyboardAvoidingView, StyleSheet } from 'react-native'
-import { Text, Block, Button, Input } from '../components'
+import { Keyboard, TextInput, ScrollView, StyleSheet } from 'react-native'
+
+import { Text, Block, Button, Input, Divider } from '../components'
 import { theme, images } from '../constants'
+
+import Icon from 'react-native-vector-icons/Entypo'
+
 
 export default class Clipboard extends Component {
     state = {
@@ -11,27 +15,33 @@ export default class Clipboard extends Component {
 
     render() {
         return (
-            <KeyboardAvoidingView behavior="height" style={styles.login}>
-                <Block color={theme.colors.white} bottom padding={[100, theme.sizes.base * 2]}>
-                    <TextInput
-                        style={{ height: 100, fontSize: 16}}
-                        multiline
-                        editable
-                        onChangeText={txt => this.setState({text: txt})}
-                        placeholder="Insert text to send"
-                        placeholderTextColor={theme.colors.gray}
-                        value={this.state.text}
-                    />
-                </Block>
 
-            </KeyboardAvoidingView>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Block style={styles.input} color={theme.colors.white} padding={[theme.sizes.base, theme.sizes.base]}>
+                    <Block row space="between">
+                        <TextInput
+                            style={{ fontSize: 16 }}
+                            multiline
+                            editable
+                            onChangeText={txt => this.setState({ text: txt })}
+                            placeholder="Insert text to send"
+                            placeholderTextColor={theme.colors.gray}
+                            value={this.state.text}
+                        />
+                        <Icon name="cross" size={30} color={theme.colors.gray3} />
+
+
+                    </Block>
+                    <Divider center style={{ width: "100%" }} ></Divider>
+                </Block>
+            </ScrollView>
+
         )
     }
 }
 
 const styles = StyleSheet.create({
-    keyAvoid: {
-        flex: 1,
-        justifyContent: 'center',
+    input: {
+    
     },
 })
