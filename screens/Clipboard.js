74de06a29dca2098a80834befcfe4f6586b1a2fc
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-19 22:49:31 +0100

    Clipboard blocks structure

--- screens/Clipboard.js
+++ screens/Clipboard.js
@@ -15,33 +15,42 @@ export default class Clipboard extends Component {
 
     render() {
         return (
-
             <ScrollView showsVerticalScrollIndicator={false}>
-                <Block style={styles.input} color={theme.colors.white} padding={[theme.sizes.base, theme.sizes.base]}>
-                    <Block row space="between">
-                        <TextInput
-                            style={{ fontSize: 16 }}
-                            multiline
-                            editable
-                            onChangeText={txt => this.setState({ text: txt })}
-                            placeholder="Insert text to send"
-                            placeholderTextColor={theme.colors.gray}
-                            value={this.state.text}
-                        />
-                        <Icon name="cross" size={30} color={theme.colors.gray3} />
-
-
+                <Block color={theme.colors.white}>
+                    <Block style={styles.input} padding={[theme.sizes.base, theme.sizes.base]}>
+                        <Block row space="between">
+                            <TextInput
+                                style={{ fontSize: 16 }}
+                                multiline
+                                editable
+                                onChangeText={txt => this.setState({ text: txt })}
+                                placeholder="Insert text to send"
+                                placeholderTextColor={theme.colors.gray}
+                                value={this.state.text}
+                            />
+                            <Icon name="cross" size={30} color={theme.colors.gray3} />
+                        </Block>
                     </Block>
-                    <Divider center style={{ width: "100%" }} ></Divider>
+
+                    <Divider center style={{ width: "100%" }} />
+                    
+                    <Block center middle>
+                        <Text>Device mini cards</Text>
+                    </Block>
+                    
+                    <Divider center style={{ width: "100%" }} />
+                    
+                    <Block row center middle>
+                        <Text>Anonymous & send Email icons</Text>
+                    </Block>
                 </Block>
             </ScrollView>
-
         )
     }
 }
 
 const styles = StyleSheet.create({
     input: {
-    
+
     },
 })
