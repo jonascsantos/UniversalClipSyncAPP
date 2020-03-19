b569d877cc993c0d1bd63e86ac5881dd3de6e537
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-03-19 21:11:09 +0100

    clipboard structure init

--- screens/Clipboard.js
+++ screens/Clipboard.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Keyboard, TextInput, KeyboardAvoidingView, StyleSheet } from 'react-native'
+import { Text, Block, Button, Input } from '../components'
+import { theme, images } from '../constants'
 
 export default class Clipboard extends Component {
+    state = {
+        text: "",
+    }
+
+
     render() {
         return (
-            <View>
-                <Text> Clipboard </Text>
-            </View>
+            <KeyboardAvoidingView behavior="height" style={styles.login}>
+                <Block color={theme.colors.white} bottom padding={[100, theme.sizes.base * 2]}>
+                    <TextInput
+                        style={{ height: 100, fontSize: 16}}
+                        multiline
+                        editable
+                        onChangeText={txt => this.setState({text: txt})}
+                        placeholder="Insert text to send"
+                        placeholderTextColor={theme.colors.gray}
+                        value={this.state.text}
+                    />
+                </Block>
+
+            </KeyboardAvoidingView>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    keyAvoid: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+})
